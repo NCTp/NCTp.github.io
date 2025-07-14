30ce64b27309adfee52417c29d1dde977f6d41ea
Mun Cheol Ho <cjfgh530@naver.com>
2025-07-14 17:20:54 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -64,7 +64,7 @@ export const profileConfig: ProfileConfig = {
 		},
 		{
 			name: "BrunchStory",
-			icon: "fa-solid fa-book",
+			icon: "fa6-brands:steam",
 			url: "https://brunch.co.kr/@ebbf1fdaf298453",
 		},
 		{
